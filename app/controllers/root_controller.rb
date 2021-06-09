487bb0341460af2ad826cab77a556f37359c12d3
Max Brosnahan <maximilianbrosnahan@gmail.com>
2021-06-09 19:24:02 +0200

    Display locations from database

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,15 @@
 class RootController < ApplicationController
   def index
+    @features = load_features
+  end
+
+  private
+
+  def load_features
+    features = Location.all.pluck(:geojson)
+    {
+      type: "FeatureGroup",
+      features: features,
+    }
   end
 end
